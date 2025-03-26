6644b62617f2de7293c1cf1ba5ae6b38b52a3717
Gabriel_Dmcn <gabriel.ceno.oliveira@gmail.com>
2025-03-26 15:21:34 -0300

    java

---
+++ src/Fundamentos/AreaCircunferencia.java
@@ -0,0 +1,8 @@
+package Fundamentos;
+
+public class AreaCircunferencia {
+    public static void main(String[] args) {
+        double raio = 3.4;
+
+    }
+}
--- src/fundamentos.java
+++
@@ -1,5 +0,0 @@
-public class fundamentos {
-    public static void main(String[] args) {
-
-    }
-}
